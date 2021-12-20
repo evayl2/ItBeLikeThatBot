4844605891b95b2c8b321960561083647bd24f5e
evayl2 <evayl2@illinois.edu>
2021-12-20 16:03:07 -0600

    finished salt

--- bot.js
+++ bot.js
@@ -2,6 +2,33 @@
 // Run dotenv
 require('dotenv').config();
 
+{ 
+  var salt = ["Oh.... I see.", 
+      "So that's how it is, uh?",
+      "That's how it's going to be, is it?",
+      "That's the way in which things are, eh?",
+      "That be how it will, is it?",
+      "Will it be how that is, huh?",
+      "That be the way in which things be the way they are, is it?",
+      "That's the way it's going to be, huh?"
+      ];
+  var bland = ["Things really do be like that, eh?", "It really do be like that sometimes, huh?"]
+
+}
+
+function randSalt() {
+  var num = Math.floor(Math.random()*8);
+  return salt[num];  
+}
+
+function randBland() {
+  var num = Math.floor(Math.random()*2);
+  return bland[num];  
+}
+
+
+
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -12,8 +39,11 @@ client.on('ready', () => {
 
 // user sends message in chat
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+    if (msg.content === '!salt') {
+      msg.reply(randSalt());
+    }
+    if (msg.content === '!bland') {
+      msg.reply(randBland());
     }
   });
 
