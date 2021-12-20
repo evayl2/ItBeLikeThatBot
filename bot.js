8f287084cd58d41be53289f9fbeb1b12fa879a64
Chang Ze-Rong <kchang1@imsa.edu>
2021-12-20 15:19:06 -0600

    it does indeed run

--- bot.js
+++ bot.js
@@ -1,73 +1,11 @@
-var Discord = require('discord.io');
+// Run dotenv
+require('dotenv').config();
 
-var logger = require('winston');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-var auth = require('./auth.json');
-
-// Configure logger settings
-
-logger.remove(logger.transports.Console);
-
-logger.add(new logger.transports.Console, {
-
-colorize: true
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-logger.level = 'debug';
-
-// Initialize Discord Bot
-
-var bot = new Discord.Client({
-
-token: auth.token,
-
-autorun: true
-
-});
-
-bot.on('ready', function (evt) {
-
-logger.info('Connected');
-
-logger.info('Logged in as: ');
-
-logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-bot.on('message', function (user, userID, channelID, message, evt) {
-
-// Our bot needs to know if it will execute a command
-
-// It will listen for messages that will start with `!`
-
-if (message.substring(0, 1) == '!') {
-
-    var args = message.substring(1).split(' ');
-
-    var cmd = args[0];
-
-
-    args = args.splice(1);
-
-    switch(cmd) {
-
-        // !ping
-
-        case 'ping':
-
-            bot.sendMessage({
-
-                to: channelID,
-
-                message: 'Pong!'
-
-            });
-
-        break;
-
-        // Just add any case commands if you want to..
-
-     }
-
- }
-});
+client.login(process.env.DISCORD_TOKEN);
