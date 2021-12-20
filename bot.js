7a49baf758525e5f619147b19074c04ded115adc
Chang Ze-Rong <kchang1@imsa.edu>
2021-12-20 15:27:31 -0600

    we have achieved ping pong

--- bot.js
+++ bot.js
@@ -8,4 +8,13 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+// user sends message in chat
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+  });
+
 client.login(process.env.DISCORD_TOKEN);
+
